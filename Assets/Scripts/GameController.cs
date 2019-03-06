9258652f13d0a27c980099f679675f0f8bd37924
sarctechie <manindgera12@gmail.com>
2019-03-06 18:33:34 +0530

    Add to project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject moleContainer;
+    public TextMesh InfoText;
+    public float spawnDuration = 1.5f;
+    public float spawnDecrement= 1.0f;
+    public float minimumSpawnTIme = 0.5f;
+    public float gameTimer= 15f;
+
+
+    private Mole[] moles;
+    private float spawnTimer= 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moles = moleContainer.GetComponentsInChildren<Mole> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer-= Time.deltaTime;
+        if(spawnTimer <= 0f){
+        moles[Random.Range(0, moles.Length)].Rise ();
+
+        spawnDuration -= spawnDecrement;
+        if(spawnDuration < minimumSpawnTIme){
+            spawnDuration= minimumSpawnTIme;
+        }
+        spawnTimer = spawnDuration;
+        gameTimer -= Time.deltaTime;
+        
+        if (gameTimer >0f){
+         InfoText.text= "Hit all the moles!\nTime: "+ Mathf.Floor(gameTimer);
+        }
+
+        else
+        {
+            InfoText.text= "Game Over!";
+        }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space")){
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if(hit.transform.GetComponent<Mole>() != null){
+                Mole mole= hit.transform.GetComponent<Mole>();
+                mole.Onhit();
+
+            }
+        }
+        }
+    }
+}
